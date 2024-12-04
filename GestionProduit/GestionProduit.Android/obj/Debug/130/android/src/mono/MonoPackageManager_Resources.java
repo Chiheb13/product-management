27f7b68b8c997250304081495c45f0b544f8cfc8
Chiheb13 <Yahyachiheb8@gmail.com>
2024-12-04 07:38:19 -1200

    added onboarding and login page

--- GestionProduit/GestionProduit.Android/obj/Debug/130/android/src/mono/MonoPackageManager_Resources.java
+++ GestionProduit/GestionProduit.Android/obj/Debug/130/android/src/mono/MonoPackageManager_Resources.java
@@ -5,6 +5,7 @@ public class MonoPackageManager_Resources {
 		"GestionProduit.Android.dll",
 		"FormsViewGroup.dll",
 		"GestionProduit.dll",
+		"Plugin.Media.dll",
 		"SQLite-net.dll",
 		"SQLitePCLRaw.batteries_v2.dll",
 		"SQLitePCLRaw.core.dll",
